129b2455a711cd376f42e834c81422aec85638fa
Gyorgy Fekete <f@pulilab.com>
2018-01-15 21:13:19 -0500

    refactor user assignment

--- woof/serializers.py
+++ woof/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
-
 from .models import Ticket
 
 
 class TicketSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Ticket
         fields = '__all__'
--- woof/views.py
+++ woof/views.py
@@ -1,13 +1,6 @@
 from rest_framework.generics import CreateAPIView
-
 from .serializers import TicketSerializer
 
 
 class TicketCreateAPIView(CreateAPIView):
     serializer_class = TicketSerializer
-
-    def create(self, request, *args, **kwargs):
-        # TODO make this a separate package
-        # TODO send email to superusers on tickets
-        request.data['user'] = request.user.id if request.user.is_authenticated() else None
-        return super().create(request, *args, **kwargs)
