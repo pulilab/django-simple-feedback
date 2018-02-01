7e9f0c3a22f3b9fdfbccef8cd0fdb449edef67dc
ilcsik <ilcsik@gmail.com>
2018-02-01 16:35:00 +0100

    Replace assignee username on admin

--- simple-feedback/admin.py
+++ simple-feedback/admin.py
@@ -9,6 +9,12 @@ class TicketAdmin(admin.ModelAdmin):
     readonly_fields = ['user', 'email', 'subject', 'text', 'meta']
     search_fields = ['subject']
 
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        field = super(TicketAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+        if db_field.name == 'assignee':
+            field.label_from_instance = lambda u: '{} <{}>'.format(u.get_full_name(), u.email)
+        return field
+
     def has_module_permission(self, request):
         """ Only available to superusers """
         return request.user.is_superuser
