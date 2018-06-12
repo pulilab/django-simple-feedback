50f0b16ec9f93bf5af2777c2b41a9d7e281c4b0b
lordfuoco <lordfuoco@gmail.com>
2018-06-12 16:08:36 +0200

    fix: use is_authenticated as a property and not as a method

--- simple-feedback/views.py
+++ simple-feedback/views.py
@@ -12,7 +12,7 @@ class TicketCreateAPIView(CreateAPIView):
     serializer_class = TicketSerializer
 
     def perform_create(self, serializer):
-        if self.request.user.is_authenticated():
+        if self.request.user.is_authenticated:
             serializer.validated_data['user'] = self.request.user
         super(TicketCreateAPIView, self).perform_create(serializer)
 
