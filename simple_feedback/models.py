8549376a466404b7f10d86c4d1d1a5e606ea45f4
Gyorgy Fekete <f@pulilab.com>
2023-04-06 15:26:24 +0200

    update meta field jsonfield import

--- simple_feedback/models.py
+++ simple_feedback/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.core import mail
 from django.contrib.auth.models import User
-from django.contrib.postgres.fields import JSONField
 from django.template import loader
 from django.dispatch import receiver
 from django.conf import settings
@@ -35,7 +34,7 @@ class Ticket(models.Model):
     text = models.TextField()
     created = models.DateTimeField(auto_now=False, auto_now_add=True)
     modified = models.DateTimeField(auto_now=True, auto_now_add=False)
-    meta = JSONField(default=dict, blank=True)
+    meta = models.JSONField(default=dict, blank=True)
 
     def __str__(self):
         return self.subject
