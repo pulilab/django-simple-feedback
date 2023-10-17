30e669be9e5fee08d39eb5588156a37afcf0bdae
Gyorgy Fekete <f@pulilab.com>
2023-10-17 14:26:37 +0200

    fix user setting

--- simple_feedback/views.py
+++ simple_feedback/views.py
@@ -14,9 +14,10 @@ class TicketCreateAPIView(CreateAPIView):
     serializer_class = TicketSerializer
 
     def perform_create(self, serializer):
+        instance = serializer.save()
         if self.request.user.is_authenticated:
-            serializer.validated_data['user'] = self.request.user
-        super(TicketCreateAPIView, self).perform_create(serializer)
+            instance.user = self.request.user
+            instance.save(update_fields=['user'])
 
 
 class TicketMetaRetrieveView(RetrieveAPIView):
